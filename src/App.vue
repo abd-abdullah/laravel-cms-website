a934d6afbe28768fdad40187ae775175d0224308
Md. Abdullah <abdullah@systechdigital.com>
2021-02-04 09:08:43 +0600

    layout done

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <body id="body">
+
+    <!-- preloader -->
+    <div id="preloader">
+      <img src="img/preloader.gif" alt="Preloader">
+    </div>
+    <!-- end preloader -->
+    <!-- header -->
+    <appHeader />
+    <!--end header -->
+    <!-- main component -->
+    <router-view/>
+    <!--end main component -->
+    <!-- footer -->
+    <appFooter />
+    <!--end footer -->
+
+  </body>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import appHeader from '@/components/layouts/Header';
+import appFooter from '@/components/layouts/Footer';
+export default {
+  components: {
+    appHeader,appFooter
   }
 }
+</script>
+
+<style>
+
 </style>
